616c592796c711254e6188df3707a3b288a080ed
Sibints2000 <sibints2000@gmail.com>
2021-09-08 11:05:24 +0530

    Sixteenth Commit

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const app = Vue.createApp({
             lastName: 'doe',
             email: 'Johndoe@gamil.com',
             gender: 'male',
-            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg'
         }
     },
-    methods :{
-        getUser() {
-            console.log(this.firstName);
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+            this.firstName = result[0].name.first,
+            this.lastName = result[0].name.last,
+            this.email = result[0].email,
+            this.gender = result[0].gender,
+            this.picture = result[0].picture.large
         }
     }
 })
